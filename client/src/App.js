4862733d259fcae151432a86763cda97fb9b29f2
Jess Daniel <jessdaniel474@gmail.com>
2019-11-01 11:24:28 -0400

    Refactored component to class and added axios call

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/players`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          data: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return <div className="App">{/* <Display /> */}</div>;
+  }
 }
 
 export default App;
